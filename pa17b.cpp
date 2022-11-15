571e8c2eb27e387bf3eb1c104f51e720b59a9e6f
Scott T. Koss <Scott.T.Koss@gmail.com>
2022-11-14 22:55:36 -0800

    can sort inputs, and add to linked list

--- pa17b.cpp
+++ pa17b.cpp
@@ -1,13 +1,44 @@
 #include "ForwardList.hpp"
+#include <iostream>
 
 
+static std::ostream& operator<<(std::ostream& output, const ForwardList<char>& info);
 using namespace std;
 
 
+
 int main(){
-    ForwardList<int> test {10, 20, 30, 40, 50};
+    char ch;
+    bool add_to_end = false;
+    ForwardList<char> info;
+    
+    while (cin >> ch && !cin.eof())
+    {
+        if(ch == '['){
+            add_to_end = false;
+            std::cin.ignore(1);
+        } else if (ch == ']') {
+            add_to_end = true;
+            std::cin.ignore(1);
+        }
 
-    cout << test.size() << endl;
+        if (add_to_end == true){
+            info.insert(info.end(), ch);
+        } else {
+            info.insert(info.begin(), ch);
+        }
+    }
 
+    cout << info;
+
+    
 return 0;
 }
+
+static std::ostream& operator<<(std::ostream& output, const ForwardList<char>& info){
+    for (auto it = info.begin() ; it != info.end(); ++it){
+        output << it->info;
+    };
+
+    return output;
+};
